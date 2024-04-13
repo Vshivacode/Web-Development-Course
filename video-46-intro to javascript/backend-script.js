ff7f161c3acacd9baaa3d579fa7a9234fa61b3cf
Vshivacode <115530603+Vshivacode@users.noreply.github.com>
2024-04-13 17:13:43 +0530

    js

---
+++ video-46-intro to javascript/backend-script.js
@@ -0,0 +1,10 @@
+// to run the javascript at backend we use nodejs 
+// t run this script file we write node and the filename
+// "node backend-script.js" in the terminal
+// and check whether you are running in the same file path the script file is or not
+console.log("code is running")
+console.log("i am at main section ")
+
+
+
+
--- video-46-intro to javascript/script.js
+++ video-46-intro to javascript/script.js
@@ -1,2 +1,32 @@
 // this is used to show a popup message when we run the code
 alert("hello world")
+
+// this is used in debugging purposes like at which line the code is stopped running we can set
+// this will be seen in console section in chrome inspect 
+console.log("code is running")
+
+
+// for example
+console.log("i am at main section ")
+
+
+var a = prompt("enter your number")
+console.log("your number is " + a)
+
+isconfirm = confirm("are you sure you want to continue ?")
+
+if(isconfirm){
+    console.log(" laptop is saving your data")
+}
+else{
+    console.log("laptop is not saving your data")
+}
+
+
+
+// to change the title of the webpage we can do this in js also
+document.title = "my first js code"
+
+
+// we can change the color also this will act as a inline css in js
+document.body.style.backgroundColor = "red"
