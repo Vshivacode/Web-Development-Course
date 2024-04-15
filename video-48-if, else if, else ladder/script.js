f193fc66cb402bf8a5f469de4eec4e0d0db7a964
Vshivacode <115530603+Vshivacode@users.noreply.github.com>
2024-04-15 18:34:26 +0530

    js

---
+++ video-48-if, else if, else ladder/script.js
@@ -0,0 +1,98 @@
+console.log("shiva")
+
+age = 234
+
+if(age>18){
+    console.log("you can drive")
+}
+else{
+    console.log("you cannot drive")
+}
+
+
+
+
+// operators
+// arithmetic operators
+// +, -, *, /, %, **, ++, --
+// exponent operator "**" that means power 
+// a = 2
+// console.log(a**5)
+
+
+
+
+// assignment operator
+// =, +=, -=, *=, /=, **=, %=
+
+// a+= 5 that means a = a+5 
+
+// console.log(a+=5)
+
+// now the a value is changed because we used assignment operator
+// console.log(a)
+
+
+
+
+
+
+// Comparison operator
+// == : equal to
+// != : not equal to
+// === : equal value and equal type 
+// !== : not equal value or not equal type
+// >, <, >=, <=, 
+// ?  = ternary operator
+
+x = 3
+y = "3"
+// x == y   returns true because their value is same but not the type 
+// x === y  returns false because value same but type is different
+
+
+
+
+
+// logical operator
+// &&, ||, !
+// p = 20
+// q = 10
+// r = 20
+// p==q && q == r returns false
+
+
+
+
+
+
+// if(age >= 18){
+//     console.log("you can drive")
+// }
+
+// else if(age == 18){
+//     console.log("you cannot drive")
+// }
+// else{
+//     console.log("i dont know")
+// }
+
+
+
+
+// using ternary operator
+let a = 20
+b = 40
+// if(a>b){
+//     let c = a + b
+//     console.log(c)
+// }
+// else{
+//     let c = a-b
+//     console.log(c)
+// }
+
+
+// we can write this code in single line using ternary operator 
+let c = a > b ? (a+b) : (a-b)
+console.log(c) 
