a4c27b2e0e22dee54a86b8f07f1d8da0bee852f9
Vshivacode <115530603+Vshivacode@users.noreply.github.com>
2024-04-20 19:03:39 +0530

    js

--- video-54-Arrays/maps,filter,reduce.js
+++ video-54-Arrays/maps,filter,reduce.js
@@ -1,5 +1,8 @@
 // square of array elements and store in a new array
-let arr = [2,354,53,64,365]
+let arr = [2,354,53,64,365, 100, 92]
+
+
+// using forof loop
 let new_arr = []
 
 for (const iterator of arr) {
@@ -7,7 +10,41 @@ for (const iterator of arr) {
     square = iterator**2
     new_arr.push(square)
 }
-console.log(new_arr)
+// console.log(new_arr)
+
+
+// we can do this in simple way using map 
+// map(index_value, index, array)
+let arr_using_map = arr.map((index_value)=>{
+    return index_value ** 2
+})
+console.log(arr_using_map)
+
+
+// we can do like this also 
+// " => " arrow function acts as a callback function that return the square of every element
+let arr_map = arr.map((index_value) => (index_value ** 2))
+console.log(arr_map)
 
 
 
+
+
+// filter() 
+// lets create a function to use this filter
+// to display the elements that are greater than some number
+const greaterThanSeventy = (element) => {
+    if(element > 70){
+        return true
+    }
+    else return false
+}
+
+console.log(arr.filter(greaterThanSeventy))
+
+
+
+
+
+
+// reduce() is like doing factorial 
