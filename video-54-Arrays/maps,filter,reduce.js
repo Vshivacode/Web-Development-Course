1140e2370d50d2c6e7434ba7fad80214e0ee4ddc
Vshivacode <115530603+Vshivacode@users.noreply.github.com>
2024-04-21 17:24:51 +0530

    js

--- video-54-Arrays/maps,filter,reduce.js
+++ video-54-Arrays/maps,filter,reduce.js
@@ -47,4 +47,29 @@ console.log(arr.filter(greaterThanSeventy))
 
 
 
-// reduce() is like doing factorial 
+// reduce() is like doing factorial to the numbers
+let arr2 = [1,2,3,4,5,]                 // first iteration - first number: 1 + second number: 2 = 3
+
+function add_num(a, b){                 // result of first iteration: 3 + third number: 3 = 6 
+    return a + b                        // result of second iteration: 6 + fourth number: 4 = 10
+}                                       // result of third iteration: 10 + fifth number: 5 = 15
+
+// we can write like also for function using arrow function
+const mul_num = (a, b)=>{
+    return a * b
+}
+
+console.log(arr2.reduce(add_num))   // o/p: 1 + 2 + 3 + 4 + 5 = 15
+
+console.log(arr2.reduce(mul_num))   // o/p: 1 * 2 * 3 * 4 * 5 = 120
+
+
+
+
+
+
+// " Array.from "
+// lets create an array from a string using "array.from"
+let role = "developer"
+console.log(Array.from(role))   // o/p: ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
